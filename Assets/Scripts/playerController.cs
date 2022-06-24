25ebda50fa99e49d870f37466cc0690eb006e5b2
steve rector <steve.rector@datadoghq.com>
2022-06-24 15:57:18 -0700

    addsworking player movement

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,17 +6,53 @@ using UnityEngine.InputSystem;
 public class playerController : MonoBehaviour
 {
     [SerializeField] InputAction movement;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float xClamp = 6.3f;
+    [SerializeField] float yClamp = 4.5f;
 
+    // stores values when player presses w, a, s, or d
+    float xInput;
+    float yInput;
     
-    // Start is called before the first frame update
-    void Start()
+ 
+    void OnEnable()
     {
-        
+        movement.Enable();
     }
+
+
+    void OnDisable()
+    {
+        movement.Disable();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessMovement();
+
     }
+
+
+    void ProcessMovement()
+    {
+        xInput = movement.ReadValue<Vector2>().x;
+        yInput = movement.ReadValue<Vector2>().y;
+
+        transform.position = new Vector2(transform.localPosition.x, transform.localPosition.y);
+
+        float xOffset = xInput * Time.deltaTime * movementSpeed;
+        float yOffset = yInput * Time.deltaTime * movementSpeed;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampXPos = Mathf.Clamp(rawXPos, -xClamp, xClamp);
+        float clampYPos = Mathf.Clamp(rawYPos, -yClamp, yClamp);
+
+
+        transform.position = new Vector2(rawXPos, rawYPos);
+    }
+
 }
